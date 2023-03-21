a762895dbaf9b45117c067c71f0e6491c28654be
AdelekeOfAfrica <adelekeofafrica@gmail.com>
2023-03-21 13:26:00 +0100

    Adding of SideNavMain MenuItemFollow MenuItem component

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -1,5 +1,6 @@
 <script setup>
 const route = useRoute();
+let showMenu = ref(false);
 
 </script>
 
@@ -35,6 +36,26 @@ const route = useRoute();
                 <div class="flex items-center">
                 <Icon class="ml-1 mr-4" name="carbon:send-alt" color="#161724" size="30"/>
                 <Icon class="mr-5" name="bx:message-detail" color="#161724" size="30"/>
+
+                <div class="relative">
+                    <button class= "mt-1"  @click="$event=>showMenu =!showMenu">
+                        <img class="rounded-full" width="33" src="https://picsum.photos/id/83/300/320" />
+                    </button>
+
+                    <div v-if="showMenu" id="popupmenu" class="absolute bg-white rounded-lg py-1.5 w-[200px] shadow-xl border top-[43px] -right-2">
+                        <NuxtLink @click ="$event =>showMenu = false" class="flex items-center justify-start py-3 px-2 hover:bg-gray-100 cursor-pointer">
+                            <Icon name="ph:user" size="20" />
+                            <span class="pl-2 font-semibold text-sm">Profile</span>
+                        </NuxtLink>
+
+                         <div class="flex items-center justify-start border-t py-3 px-1.5 hover:bg-gray-100 cursor-pointer">
+                            <Icon name="ic:outline-login" size="20" />
+                            <span class="pl-2 font-semibold text-sm">Log out</span>
+                        </div>
+                    </div>
+                </div>
+
+
                 </div>
             </div>
         </div>
