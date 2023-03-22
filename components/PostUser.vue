a24ae2930f86b2b9780c965cafc2b61faf874b6b
AdelekeOfAfrica <adelekeofafrica@gmail.com>
2023-03-22 17:28:56 +0100

    profile page

---
+++ components/PostUser.vue
@@ -0,0 +1,54 @@
+<script setup>
+const route = useRoute();
+const router = useRouter();
+
+let video = ref(null);
+let isLoaded = ref(false);
+
+onMounted(()=>{ //if video is not empty fetch url //
+    if(video.value) {
+        video.value.addEventListener('loadeddata', (e)=>{
+            if(e.target){
+                setTimeout(()=>{
+                    isLoaded.value = true
+                }, 200)
+            }
+        })
+    }
+})
+
+
+onBeforeUnmount(() =>{
+    video.value.pause()
+    video.value.currentTime = 0 
+    video.value.src = ''
+})
+
+const isHover =(bool) => {//pause and play of the video 
+    if (bool) {
+        video.value.play()
+    } else{
+        video.value.pause()
+    }
+}
+</script>
+
+<template>
+    <div @mouseenter = "$event =>isHover(true)" @mouseleave = "$event =>isHover(false)" class="relative brightness-90 hover:brightness-[1.1] cursor-pointer">
+        <div v-if="!isLoaded" class="absolute flex items-center justify-center top-0 left-0 aspect-[3/4] w-full object-cover rounded-md bg-black">
+            <Icon class="animate-spin ml-1" name="mingcute:loading-line" size="100" color="#ffffff" />
+        </div>
+
+        <div>
+            <video ref="video" muted loop class="aspect-[3/4] object-cover rounded-md" src="/Reeds.mp4"/>
+        </div>
+
+        <div class="px-1">
+            <div class="text-gray-700 text-[15px] pt-1 break-words">This is some test </div>
+        </div>
+        <div class="flex items-center -ml-1 text-gray-600 font-bold text-xs">
+            <Icon name="gg:loadbar-sound" size="20" /> 3%
+            <Icon name="tabler:alert-circle" size="16" class="ml-1" />
+        </div>
+    </div>
+</template>
